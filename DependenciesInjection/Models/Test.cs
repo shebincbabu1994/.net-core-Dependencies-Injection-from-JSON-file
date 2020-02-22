d509cb6b09adf15fd28ef7616d4db661694ebcb1
Shebin <shebincbabu1994@outlook.com>
2020-02-22 15:07:10 +0530

    Complete dependency injection

---
+++ DependenciesInjection/Models/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependenciesInjection.Models
+{
+    public class Test:ITest
+    {
+        public string DoSomething(string parameter)
+        {
+            return $"Message from Test with { parameter }";
+        }
+    }
+}
